474f17987508a877e761f9ab55dbc169f1406679
Joachim Breitner <mail@joachim-breitner.de>
2023-07-12 15:15:19 +0200

    Use stdin

--- doi2bib.py
+++ doi2bib.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
+import sys
+import yaml
 import requests
 import requests_cache
 import re
-import yaml
 
 requests_cache.install_cache(backend='sqlite')
 
-data = yaml.safe_load(open('doibib.yaml'))
+data = yaml.safe_load(sys.stdin)
 
 for key, doi in data['entries'].items():
     bib = requests.get(f"https://dblp.org/doi/{doi}.bib").text
     bib = re.sub('{DBLP.*,', '{' + key + ',', bib)
-
     for subs in data['subs']:
         bib = re.sub(subs['replace'], subs['with'], bib)
-
-    print(bib) 
+    print(bib)
