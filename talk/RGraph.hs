c93f4500a571870adfc385c21d6ba0867e1225cd
Joachim Breitner <mail@joachim-breitner.de>
2023-08-30 14:43:58 +0200

    Refine

--- talk/RGraph.hs
+++ talk/RGraph.hs
@@ -7,12 +7,12 @@ import qualified Data.Recursive.Set as RS
 type Graph = M.Map Int [Int]
 
 rtrans :: Graph -> Graph
-rtrans g = M.map S.toList reaches
+rtrans g = M.map (S.toList . RS.get) reaches
   where
-    reaches :: M.Map Int (S.Set Int)
+    reaches :: M.Map Int (RS.RSet Int)
     reaches = M.mapWithKey f g
 
-    f v vs = S.insert v (S.unions [ reaches M.! v' | v' <- vs ])
+    f v vs = RS.insert v (RS.unions [ reaches M.! v' | v' <- vs ])
 
 
 graph1 :: Graph
